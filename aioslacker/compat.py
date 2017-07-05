d8bbd8d7974e249cb66a16ff6ad5c8f63b92f059
froosty <salyukkonstantin@gmail.com>
2017-07-05 13:54:10 +0300

    fix aiohttp bug & add different version of aiohttp to travis

--- aioslacker/compat.py
+++ aioslacker/compat.py
@@ -1,8 +1,6 @@
 import asyncio
 import sys
 
-import aiohttp
-
 try:
     from asyncio import ensure_future
 except ImportError:
@@ -10,5 +8,3 @@ except ImportError:
 
 PY_350 = sys.version_info >= (3, 5, 0)
 PY_352 = sys.version_info >= (3, 5, 2)
-
-AIOHTTP_2 = aiohttp.__version__ >= '2.0.0'
